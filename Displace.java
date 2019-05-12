751ec6165e94f62e15307f32606f413f24fef501
hjh0915 <623299157@qq.com>
2019-05-12 19:19:45 +0800

    学习数组

---
+++ Displace.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Displace {
+    public static void main(String[] args) {
+        int arr[] = new int[] {45, 12, 2, 10};
+        Arrays.fill(arr, 1, 3, 8);
+        for (int i=0; i<arr.length; i++) {
+            System.out.println("第" + i + "个元素是：" + arr[i]);
+        }
+    }
+}
